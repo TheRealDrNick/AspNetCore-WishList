d9f61843ef8c6761592ad1de1aeb9fd4e6abb3ff
Nicolas Wittmann <Nicolas.Wittmann@ke-elektronik.de>
2020-08-25 09:19:28 +0200

    Complete module

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WishList.Models;
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private static DbContextOptions options;
+
+        public ApplicationDbContext(DbContextOptions dbContextOptions) : base(options)
+        {
+
+        }
+        public DbSet<Item> Items { get; set; }
+    }
+}
